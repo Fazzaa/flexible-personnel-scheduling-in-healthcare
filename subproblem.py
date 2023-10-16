bd6c1b93fba9e7c2bccb7026fa3a87f49bfe97c6
Fabio Luciani <lu.fa.98@gmail.com>
2023-10-16 16:38:38 +0200

    subproblem with penalties

--- subproblem.py
+++ subproblem.py
@@ -1,59 +1,76 @@
-import gurobipy as gp
+from gurobipy import *
 
-L = [8] # due tipi di turni
-p = 48 # numero di periodi totali
-periods = [i for i in range(p) if i%8==0]
-n = 2 # quanti workshift deve fare ognuno nel tour
+'''def cost(slack_five_days):
+    somma = 0
+    for j in starting_time[:-15]:
+        somma += slack_five_days[j].X
+    return somma'''
 
 def subproblem(pi):
-    print("INIZIO SUBPROBLEM")
-    subproblem = gp.Model()
-    X = {} #48 ore, ogni 8 ore può iniziare turno
+     
+    n = 10 # numero di turni che devono essere lavorati nel periodo analizzato
+    workshift_len = 8
+    p = 24
+    total_time = 336
+    sevendays = 168
+
+    starting_time = [i for i in range(total_time) if i%8==0] # [0,8,16,24,32,40]
+    print(starting_time)
+    #make subproblem
+    subproblem=Model()
     
-    for index, value in enumerate(periods):
-        X[value] = subproblem.addVar(obj = pi[index], vtype=gp.GRB.BINARY, name=f"X_{value}")
-        #Z[j] = subproblem.addVar(vtype=gp.GRB.BINARY, name=f"Z_{j}")
+    X = {} # Vale 1 se inizi a lavorare a pedice j
+    for idx, value in enumerate(starting_time):
+        X[value] = subproblem.addVar(0,1, 1-pi[idx], vtype = GRB.BINARY, name=f"X_{value}")
 
-    #for l in L:
-    #    s[l] = 1 #subproblem.addVar(vtype=gp.GRB.CONTINUOUS, name="s")
-
-    subproblem.update()
-
-    somma = 0
-    for j in periods:
-        somma += X[j]
-
-    subproblem.addConstr(somma == n, name="primo_vincolo")
-
-
-    #for j in periods:
-    #    if j != 40:
-    #        for l in L:
-    #            subproblem.addConstr(Z[j+l] == s[l]*X[j], name="secondo_vincolo")
+    subproblem.addConstr(quicksum(X[j] for j in starting_time) == n)
     
-    
-    for j in periods[:-2]: # j = 32
+    for j in starting_time[:-2]: # j = 32
         somma = 0
-        idx = (j+16) # idx = 32+12 = 44
-        for i in range(j, idx+1):
+        idx = (j+p) # idx = 32+12 = 44
+        for i in range(j, idx):
             if i % 8 == 0:
                 somma += X[i]
         subproblem.addConstr(somma <= 1, name="secondo_vincolo")
     
+    slack_five_days = {}
+    for j in starting_time[:-21]:
+        slack_five_days[j] = subproblem.addVar(0, vtype=GRB.CONTINUOUS, name="slack_var")
+        subproblem.update()
+        somma = 0
+        idx = (j+sevendays)
+        for i in range(j, sevendays):
+            if i % workshift_len == 0:
+                somma += X[i]
+        subproblem.addConstr(somma-slack_five_days[j] <= 5, name="terzo_vincolo")
+
+    subproblem.setObjective(quicksum(slack_five_days[i] for i in starting_time[:-21]), sense=GRB.MINIMIZE)
+
     subproblem.update()
     subproblem.optimize()
-
-    shift = [0]*48
-    for i in range(48):
-        if i%8==0:
-            if X[i].X == 1:
-                shift[i:(i+8)] = [1]*8
-    #print(shift)
-    #print(subproblem.ObjVal)
     
-    return subproblem.Objval, shift 
-
-if __name__=="__main__":
+    shift = [0]*total_time
+    for idx in range(total_time):
+        if idx % workshift_len == 0:
+            if X[idx].X == 1:
+                shift[idx] = 1
+    
+    for i in range(len(shift)):
+        if i != 0 and i % 24 == 0:
+            print("\n")    
+        
+        print(shift[i], end="")
+    #print(shift)
+    print(slack_five_days)
+    return subproblem.objVal, shift
+    
+    
+    
+    
+if __name__ == '__main__':
     import random
-    subproblem([random.randint(-10,10) for i in range(48)])
+    total_time = 336
+    starting_time = [i for i in range(total_time) if i%8==0]
+    shift = subproblem([random.randint(-10,10) for i in range(len(starting_time))])
+    #print(shift)
     
