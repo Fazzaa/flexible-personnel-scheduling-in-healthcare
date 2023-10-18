1f16d7589e411db69ba8ecd39e9511b459eeceb0
Fabio Luciani <lu.fa.98@gmail.com>
2023-10-18 12:11:43 +0200

    cose

--- masterproblem.py
+++ masterproblem.py
@@ -3,6 +3,8 @@ from subproblem import subproblem
 import time
 import numpy as np
 
+  
+
 def diff(a,b):
     if b != []:
         result = []
@@ -11,23 +13,26 @@ def diff(a,b):
         return result
     return a
 
+  
+
 def compute_vec_distance(tour_pool, requested_coverage):
-    #tour = [(tour, Z), ...] --> [([1,0,0,0,0,0], [1,1,1,1,1,1,1,0,0,0]), ...]
+#tour = [(tour, Z), ...] --> [([1,0,0,0,0,0], [1,1,1,1,1,1,1,0,0,0]), ...]
     remaining_coverage = requested_coverage
     for i in range(len(tour_pool)):
         temp = np.array(tour_pool[i][1])*int(Y[i].X)
+
         remaining_coverage = diff(remaining_coverage, temp)
+
     return remaining_coverage
 
 t1=time.time()
 I = 10
 periods = 336
 
-requested_coverage = [3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 1, 1, 1, 1, 1, 1]*7 
+requested_coverage = [3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 1, 1, 1, 1, 1, 1]*7
 remaining_coverage = requested_coverage
 
 tour_pool = []
-
 masterproblem = Model()
 
 Y = {}
@@ -35,51 +40,40 @@ for i in range(I):
     Y[i] = masterproblem.addVar(0,1, vtype=GRB.BINARY, name=f"Y_{i}")
     masterproblem.addConstr(Y[i] <= 1)
 
-masterproblem.addConstr(quicksum(Y[i] for i in range(I)) <= I)
+masterproblem.addConstr(quicksum(Y) <= I)
 
 #objective_function = quicksum((requested_coverage[i] - tupla[1][i] * Y[j]) ** 2 for i in range(len(requested_coverage)) for j, tupla in enumerate(tour_pool))
+
 #masterproblem.setObjective(objective_function, sense=GRB.MINIMIZE)
+
 #print(f"SONO QUIIIIIIIIIIIIIIIIIIIIIIIIII \t\t\t{objective_function}")
 
 masterproblem.update()
-
 iteration = 0
-while iteration < 10:
-    masterproblem.optimize()
-    tour, Z = subproblem(remaining_coverage)
-    tour_pool.append((tour, Z))
+while iteration < 30:
+    print(f"Iterazione numero: {iteration}")
+    if iteration >= 10:
+        Y[iteration] = masterproblem.addVar(0,1, vtype=GRB.BINARY, name=f"Y_{iteration}")
+
     masterproblem.update()
+    masterproblem.optimize()
+    n, tour, Z = subproblem(remaining_coverage)
+
+    if tour == [] and Z == []:
+        break
+
+    print(f"Questo è l'objVal del sottoproblema {n}")
+    tour_pool.append((tour, Z))
     remaining_coverage = compute_vec_distance(tour_pool, remaining_coverage)
-    
+
     if tour_pool:
         objective_function = quicksum((requested_coverage[i] - tupla[1][i] * Y[j]) ** 2 for i in range(len(requested_coverage)) for j, tupla in enumerate(tour_pool))
         masterproblem.setObjective(objective_function, sense=GRB.MINIMIZE)
 
-    if masterproblem.status == GRB.OPTIMAL:
-        print(Y)
+    '''if masterproblem.status == GRB.OPTIMAL:
 
+    print(Y)'''
     iteration+=1
 
-   
-'''for i in range(len(tour_pool)):
-    for k in range(len(tour_pool[i][0])):
-        if k != 0 and k % 24 == 0:
-            print("\n")    
-        
-        print(tour_pool[i][0][k], end="")'''
-
 print(remaining_coverage)
-chosen_tours = [tour_pool[i][0] for i in range(len(tour_pool)) if Y[i].X == 1]
-
-print(chosen_tours)
-
-'''
-Do
-    For each local search structure:
-        Do:
-            Execute current local search -> SUBPROBLEM?
-        Repeat until no improvements are found -> ? 
-    Next local search structure ?
-Repeat until no more improvements have been found with any local search
-'''
-
+print(f"In: {iteration} iterazioni")
--- subproblem.py
+++ subproblem.py
@@ -1,91 +1,76 @@
 from gurobipy import *
 
-'''def cost(slack_five_days):
-    somma = 0
-    for j in starting_time[:-15]:
-        somma += slack_five_days[j].X
-    return somma'''
+  
 
 def subproblem(pi):
-     
+    #print(pi)
     n = 10 # numero di turni che devono essere lavorati nel periodo analizzato
     workshift_len = 8
     p = 24
     total_time = 336
     sevendays = 168
-    service_coverage_value = 1 #quanto 
+    service_coverage_value = 1 #quanto
     starting_time = [i for i in range(total_time) if i%8==0] # [0,8,16,24,32,40]
-    
     subproblem=Model()
-    
+
     X = {} # Vale 1 se inizi a lavorare a pedice j
+
     for idx, value in enumerate(starting_time):
-        X[value] = subproblem.addVar(0,1, 1-pi[idx], vtype = GRB.BINARY, name=f"X_{value}")
+        X[value] = subproblem.addVar(0,1, vtype = GRB.BINARY, name=f"X_{value}")
 
     subproblem.addConstr(quicksum(X[j] for j in starting_time) == n)
 
     Z = [0]*total_time
+    slack_1 = [0]*total_time
+    slack_2 = [0]*total_time
     for i in range(total_time):
         Z[i] = subproblem.addVar(0, 1, vtype=GRB.BINARY, name=f"Z_{i}")
+        slack_1[i] = subproblem.addVar(0, vtype=GRB.CONTINUOUS, name=f"slack_{i}")
+        slack_2[i] = subproblem.addVar(0, vtype=GRB.CONTINUOUS, name=f"slack_{i}")
 
-     
     for j in starting_time:
         for i in range(j,j+workshift_len):
             subproblem.addConstr(Z[i] == (X[j]*service_coverage_value), name="coverage_constraint")
     subproblem.update()
-    
-    for j in starting_time[:-2]: # j = 32
+
+    #X [1,0,0,0,0,0,0,0] -> Z=[1,1,1,1,1,1,1,1]
+
+    #pi = [3,3,3,3,3,3,3,3] - [1,1,1,1,1,1,1,1] + [..............] = 0
+    for i in range(len(pi)):
+        subproblem.addConstr(pi[i] - Z[i] + slack_1[i] >= 0, name=f"under_coverage_constraint_{i}")
+        subproblem.addConstr(pi[i] - Z[i] - slack_2[i]<= 0, name=f"over_coverage_constraint_{i}")
+
+    subproblem.setObjective(quicksum(slack_1[i] for i in range(len(slack_1)))*1 + quicksum(slack_2[i] for i in range(len(slack_2)))*1, sense = GRB.MINIMIZE)
+
+    for j in starting_time[:-2]:
         somma = 0
-        idx = (j+p) # idx = 32+12 = 44
+        idx = (j+p)
         for i in range(j, idx):
             if i % 8 == 0:
                 somma += X[i]
+
         subproblem.addConstr(somma <= 1, name="secondo_vincolo")
     subproblem.update()
-
-
-    slack_seven_days = {}
-    for j in starting_time[:-21]:
-        slack_seven_days[j] = subproblem.addVar(0, vtype=GRB.CONTINUOUS, name="slack_var")
-        subproblem.update()
-        somma = 0
-        idx = (j+sevendays)
-        for i in range(j, sevendays):
-            if i % workshift_len == 0:
-                somma += X[i]
-        subproblem.addConstr(somma-slack_seven_days[j] <= 5, name="terzo_vincolo")
-
-    subproblem.setObjective(quicksum(slack_seven_days[i] for i in starting_time[:-21]), sense=GRB.MINIMIZE)
-
-    subproblem.update()
     subproblem.optimize()
-    
-    shift = [0]*total_time
-    for idx in range(total_time):
-        if idx % workshift_len == 0:
-            if X[idx].X == 1:
-                shift[idx] = 1
-    
-    coverage =  [int(elem.X) for elem in Z]
-    '''for i in range(len(shift)):
-        if i != 0 and i % 24 == 0:
-            print("\n")    
-        
-        print(shift[i], end="")'''
-    #print(shift)
 
-    #for j in starting_time[:-21]:
-    #    print(slack_seven_days[j].X)
-    
-    return shift, coverage
-    
-    
-    
-    
+    if subproblem.status == GRB.INFEASIBLE:
+        print("INFEASIBLE")
+        return 0, [], []
+
+    else:
+        shift = [0]*total_time
+        for idx in range(total_time):
+            if idx % workshift_len == 0:
+                if X[idx].X == 1:
+                    shift[idx] = 1
+
+    coverage = [int(elem.X) for elem in Z]
+    print(f"\nObjVal: {subproblem.objVal}")
+    return subproblem.objVal, shift, coverage
+
 if __name__ == '__main__':
     import random
     total_time = 336
     starting_time = [i for i in range(total_time) if i%8==0]
-    shift = subproblem([random.randint(-10,10) for i in range(len(starting_time))])
+    shift = subproblem([random.randint(0,10) for i in range(len(starting_time))])
     #print(shift)
-    
