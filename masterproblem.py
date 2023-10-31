9003159601df3a6616c57b42611e41f2e8495563
Fabio Luciani <lu.fa.98@gmail.com>
2023-10-31 09:47:32 +0100

    Refattorizzazione del codice.

--- masterproblem.py
+++ masterproblem.py
@@ -5,12 +5,9 @@ import numpy as np
 import random as rn
 
 def diff(a,b):
-    result = []
-    for i in range(len(a)):
-        result.append(a[i]-b[i])
-    return result
+    return [a[i]-b[i] for i in range(len(a))]
 
-def compute_vec_distance(tour_pool, requested_coverage):
+def compute_vec_distance(tour_pool, requested_coverage, Y):
 #tour = [(tour, Z), ...] --> [([1,0,0,0,0,0], [1,1,1,1,1,1,1,0,0,0]), ...]
     remaining_coverage = requested_coverage
     for i in range(len(tour_pool)):
@@ -30,71 +27,76 @@ def get_random_coverage(days):
         i += 1
     return day_coverage*days
 
-t1=time.time()
-I = 13
-periods = 336
-day_coverage = []
+def masterproblem(n_workers, requested_coverage):
+    remaining_coverage = requested_coverage
+    tour_pool = []
+    masterproblem = Model()
 
-#requested_coverage = get_random_coverage(14)
-requested_coverage = [2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4]*14
-remaining_coverage = requested_coverage
-
-tour_pool = []
-masterproblem = Model()
-
-#* Y[i] = 1 se il tour i-esimo è selezionato, 0 altrimenti
-Y = {}
-for i in range(I):
-    Y[i] = masterproblem.addVar(0,1, vtype=GRB.BINARY, name=f"Y_{i}") 
-    
-masterproblem.update()
-
-iteration = 0
-prev_mp_obj_val = 0.1
-while True:
-    print(f"Iterazione numero: {iteration}")
-    
-    if iteration >= I:
-        Y[iteration] = masterproblem.addVar(0,1, vtype=GRB.BINARY, name=f"Y_{iteration}")
-    
-    tour, Z = subproblem(remaining_coverage)
-    tour_pool.append((tour, Z))
-
-    masterproblem.setObjective(objfn(requested_coverage, tour_pool, Y), sense=GRB.MINIMIZE)
-    masterproblem.addConstr(quicksum(Y[i] for i in range(len(Y))) <= I)
-
+    #* Y[i] = 1 se il tour i-esimo è selezionato, 0 altrimenti
+    Y = {}
+    for i in range(n_workers):
+        Y[i] = masterproblem.addVar(0,1, vtype=GRB.BINARY, name=f"Y_{i}") 
+        
     masterproblem.update()
-    masterproblem.optimize()
-    remaining_coverage = compute_vec_distance(tour_pool, requested_coverage)
-    
-    if (masterproblem.objVal / prev_mp_obj_val) < 1.05:
-        break
-    else:
-        prev_mp_obj_val = masterproblem.objVal
-    
-    iteration+=1
-t2 = time.time()
+
+    iteration = 0
+    prev_mp_obj_val = 0.1
+    while True:
+        print(f"Iterazione numero: {iteration}")
+        
+        if iteration >= n_workers:
+            Y[iteration] = masterproblem.addVar(0,1, vtype=GRB.BINARY, name=f"Y_{iteration}")
+        
+        tour, Z = subproblem(remaining_coverage)
+        tour_pool.append((tour, Z))
+
+        masterproblem.setObjective(objfn(requested_coverage, tour_pool, Y), sense=GRB.MINIMIZE)
+        masterproblem.addConstr(quicksum(Y[i] for i in range(len(Y))) <= n_workers)
+
+        masterproblem.update()
+        masterproblem.optimize()
+        remaining_coverage = compute_vec_distance(tour_pool, requested_coverage, Y)
+        
+        if (masterproblem.objVal / prev_mp_obj_val) < 1.05:
+            break
+        else:
+            prev_mp_obj_val = masterproblem.objVal
+        
+        iteration+=1
 
 
-for i in range(len(Y)):
-    print(f"Y_{i}: {Y[i].X}")
-    '''for j in range(0, len(tour_pool[i][0])):
+
+    for i in range(len(Y)):
+        print(f"Y_{i}: {Y[i].X}")
+        '''for j in range(0, len(tour_pool[i][0])):
+            if j % 24 != 0:
+                print(tour_pool[i][0][j], end="")
+            else:
+                print("\n")
+                print(tour_pool[i][0][j], end ="")'''
+
+    print(requested_coverage)
+    
+    print("---------------------PRINTING REMAINING COVERAGE AFTER SCHEDULING---------------------------")
+    for j in range(0, len(remaining_coverage)):
         if j % 24 != 0:
-            print(tour_pool[i][0][j], end="")
+            print(f"[{remaining_coverage[j]}]", end="")
         else:
             print("\n")
-            print(tour_pool[i][0][j], end ="")'''
+            print(f"[{remaining_coverage[j]}]", end ="")
+    
+    print(sum(remaining_coverage))
+    print(f"In: {iteration} iterazioni")
 
-print(requested_coverage)
-print("------------------------------------------------")
-for j in range(0, len(remaining_coverage)):
-    if j % 24 != 0:
-        print(f"[{remaining_coverage[j]}]", end="")
-    else:
-        print("\n")
-        print(f"[{remaining_coverage[j]}]", end ="")
-#print(remaining_coverage)
-print(sum(remaining_coverage))
 
-print(f"In: {iteration} iterazioni")
-print(f"In: {round(t2-t1, 2)} secondi")
+
+if __name__ == "__main__":
+    n_workers = 13
+    #? planning_horizon = 336 - useless ?
+    requested_coverage = [2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4]*14
+    #requested_coverage = get_random_coverage(14)
+    
+    t1=time.time()
+    masterproblem(n_workers, requested_coverage)
+    t2 = time.time()
+    print(f"In: {round(t2-t1, 2)} secondi")
