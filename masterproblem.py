6addcba4d5e510d5c0e742c0efeddf0c8f7e18c5
Fabio Luciani <lu.fa.98@gmail.com>
2023-11-03 18:12:33 +0100

    added charts for better output visualization

--- masterproblem.py
+++ masterproblem.py
@@ -3,12 +3,12 @@ from subproblem import subproblem
 import time
 import numpy as np
 import random as rn
+import matplotlib.pyplot as plt 
 
 def diff(a,b):
     return [a[i]-b[i] for i in range(len(a))]
 
 def compute_vec_distance(tour_pool, requested_coverage, Y):
-#tour = [(tour, Z), ...] --> [([1,0,0,0,0,0], [1,1,1,1,1,1,1,0,0,0]), ...]
     remaining_coverage = requested_coverage
     for i in range(len(tour_pool)):
         temp = np.array(tour_pool[i][1])*int(Y[i].X)
@@ -64,16 +64,8 @@ def masterproblem(n_workers, requested_coverage):
         
         iteration+=1
 
-
-
     for i in range(len(Y)):
         print(f"Y_{i}: {Y[i].X}")
-        '''for j in range(0, len(tour_pool[i][0])):
-            if j % 24 != 0:
-                print(tour_pool[i][0][j], end="")
-            else:
-                print("\n")
-                print(tour_pool[i][0][j], end ="")'''
 
     print(requested_coverage)
     
@@ -87,16 +79,35 @@ def masterproblem(n_workers, requested_coverage):
     
     print(sum(remaining_coverage))
     print(f"In: {iteration} iterazioni")
-
+    return remaining_coverage
 
 
 if __name__ == "__main__":
     n_workers = 13
-    #? planning_horizon = 336 - useless ?
-    requested_coverage = [2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4]*14
+    requested_coverage = [3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,4,4,4,4,4,4,4,4]*14
     #requested_coverage = get_random_coverage(14)
     
     t1=time.time()
-    masterproblem(n_workers, requested_coverage)
+    output = masterproblem(n_workers, requested_coverage)
     t2 = time.time()
     print(f"In: {round(t2-t1, 2)} secondi")
+    output = [output[i] for i in range(len(output)) if i % 8 == 0]
+    
+    colors = []
+    for elem in output:
+        if elem < 0:
+            colors.append('g')
+        elif elem == 0:
+            colors.append('b')
+        else:
+            colors.append('r')
+    
+    plt.scatter(list(range(1,43)), output, c=colors)
+    plt.axhline(y=0, color='#999999', linestyle='-') 
+    plt.ylim(-4, 4)
+    plt.xlabel("Work shift")
+    plt.text(0.5, 0.9, 'Undercoverage', color='red', ha='center', va='center', transform=plt.gca().transAxes)
+    plt.text(0.5, 0.1, 'Overcoverage', color='green', ha='center', va='center', transform=plt.gca().transAxes)
+
+    plt.title("Work shift for planning horizon")
+    plt.show()
